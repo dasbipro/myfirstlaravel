9fef210f897778cd70291ec56d26c41e922f3528
Bipon Das <dasbipro16@gmail.com>
2020-06-28 20:25:31 +0600

    Modified

--- database/migrations/2020_06_28_052012_create_students_table.php
+++ database/migrations/2020_06_28_052012_create_students_table.php
@@ -15,6 +15,9 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->date('dob');
             $table->timestamps();
         });
     }
